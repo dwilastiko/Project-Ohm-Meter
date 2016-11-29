a5e48c7e77a3c21e86b06b5d0ebcc4b123ccefdd
dwilastiko <lastiko68@gmail.com>
2016-11-29 16:30:19 +0700

    Add files via upload

---
+++ project_resistor.ino
@@ -0,0 +1,28 @@
+#include<math.h>
+float Vin=5,R1=330,R2,V1,V2,adc;
+int pinRes = 0;
+
+void setup()
+
+{
+Serial.begin(9600); // setup koneksi serial
+
+}
+
+void loop() {
+
+adc = analogRead(pinRes); 
+V2= (adc*Vin)/1023.0;
+V1= Vin - V2;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+R2= (V2*R1/V1);
+
+Serial.print("R2: ");
+Serial.println(R2);
+Serial.print("Vout: ");
+Serial.println(V2);
+Serial.print("ADC: ");
+Serial.println(ADC); 
+
+delay(1000); 
+
+}
